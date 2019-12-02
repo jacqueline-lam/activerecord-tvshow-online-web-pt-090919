460a78699fe666e9e819fa4cb93f23cc2d616239
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-02 05:08:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,11 @@ class Show < ActiveRecord::Base
   # Returns the highest rating of all the TV shows
   def self.highest_rating
     Show.maximum(:rating)
-    binding.pry
+    #=> 10
   end
   
   # Returns the tv show with the highest rating
+  def self.most_popular_show
+    Show.
+  end
 end

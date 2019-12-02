70be409f8c377954375703d9d8f375baaabb349c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-02 05:16:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -31,6 +31,7 @@ class Show < ActiveRecord::Base
   # Returns an array of all of the shows with a rating above 5
   def self.popular_shows
     Show.where('rating > 5')
+    binding.pry
   end
   
   # Returns an array of all of the shows, listed in alphabetical order
